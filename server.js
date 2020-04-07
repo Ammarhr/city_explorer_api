6cd4ca26e716f59407f07d95edc545de1820b234
Ammarhr <hariry49@gmail.com>
2020-04-07 21:45:23 +0300

    done with database

--- server.js
+++ server.js
@@ -7,15 +7,22 @@ const cors = require('cors');
 
 const superagent = require('superagent');
 
-const PORT = process.env.PORT;
+const pg = require('pg');
+
+const PORT = process.env.PORT || 3030;
 
 const server = express();
 
 server.use(cors());
 
-server.listen(PORT, () => {
-    console.log(`Listening on PORT${PORT}`);
-});
+const client = new pg.Client(process.env.DATABASE_URL);
+
+client.connect()
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log(`Listening on PORT${PORT}`);
+        })
+    })
 
 server.get('/', (request, response) => {
     response.status(200).send('it\'s working');
@@ -25,14 +32,27 @@ server.get('/location', locationHandler);
 server.get('/weather', weatherHandler);
 server.get('/trails', trailsHandler);
 
+
 function locationHandler(request, response) {
     const city = request.query.city;
-    let key = process.env.GEOCODE_API_KEY;
-    const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-    superagent.get(url).then((geoData) => {
-        const locationData = new Location(city, geoData.body);
-        response.status(200).json(locationData);
-    });
+    let sql = 'SELECT * FROM locationInfo WHERE search_query = $1';
+    const safeValue = [city];
+    client.query(sql, safeValue)
+        .then(dataResult => {
+            if (dataResult.rowCount > 0) { //to check if the search_query is exist in the databaseor not
+                response.send(dataResult.rows[0])
+            } else {
+                let key = process.env.GEOCODE_API_KEY;
+                const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+                superagent.get(url).then((geoData) => {
+                    const locationData = new Location(city, geoData.body);
+                    let sql = 'INSERT INTO locationInfo (search_query, formatted_query,latitude,longitude) VALUES ($1 ,$2,$3,$4)'
+                    let safeValue = [locationData.search_query, locationData.formatted_query, locationData.latitude, locationData.longitude];
+                    client.query(sql, safeValue);
+                    response.status(200).json(locationData);
+                });
+            }
+        })
 }
 
 function Location(city, geoData) {
@@ -42,12 +62,11 @@ function Location(city, geoData) {
     this.longitude = geoData[0].lon;
 }
 
-
 function weatherHandler(request, response) {
     let weatherSummary = [];
     const city = request.query.search_query;
     let key = process.env.WEATHER_API_KEY;
-    console.log('ddddddddddddddddddddd', key);
+    //   console.log('ddddddddddddddddddddd', key);
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
     superagent.get(url).then((weatherData) => {
         weatherData.body.data.map((theWeather) => {
@@ -62,7 +81,6 @@ function Weather(dataOfWeather) {
     this.time = dataOfWeather.valid_date;
     this.forecast = dataOfWeather.weather.description;
 }
-
 
 function trailsHandler(request, response) {
     let trailsArray = [];
